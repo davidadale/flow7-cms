0d27dce17d4d4710333de6a6987c30d8a3da09b3
david dale <david@flow7.net>
2012-02-22 09:49:20 -0500

    add bootstrap and new security stuff

---
+++ app/models/SiteUser.java
@@ -0,0 +1,11 @@
+package models;
+
+import siena.*;
+
+public class SiteUser extends Model{
+    
+    public static String username;
+    public static String password;
+    public static boolean enabled;
+    
+}

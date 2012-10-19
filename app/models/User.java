d60492aae569dc9d9e2d20f868eaa526481e5129
David Dale <david@flow7.net>
2012-10-19 15:13:09 -0400

    adding user model

---
+++ app/models/User.java
@@ -0,0 +1,31 @@
+package models;
+
+import siena.*;
+
+public class User extends Model{
+	@Id
+	public Long id;
+	public String username;
+	public String password;
+
+	public User(){
+
+	}
+
+	public User(String username,String password){
+		this.username = username;
+		this.password = password;
+	}
+
+   	public static Query<User> all() {
+        return Model.all(User.class);
+    }	
+
+    public static User findByUsername(String username){
+		return all().filter("username", username).get();    	
+    }
+
+    public static int count(){
+		return all().count();
+	}
+}
